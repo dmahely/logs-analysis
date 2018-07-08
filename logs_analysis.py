8fb9e1de8b285f6675a672d39f1523e7db834510
Doaa Mahely <dom.k9@hotmail.com>
2018-07-08 22:31:59 +0400

    Days with most errors query

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+
 import psycopg2
 
 # database name declared as a constant
 DBNAME = 'dbname=news'
+# creates and opens a text file called output
+output = open('output.txt', 'w')
+
 
 # takes a query and returns the results
 def get_query_results(query):
@@ -21,16 +26,35 @@ def get_query_results(query):
 # PART ONE:  What are the most popular three articles of all time?
 article_views = 'SELECT * FROM article_views;'
 most_popular_articles = get_query_results(article_views)
-print('\nThe most popular three articles of all time are: ')
-print('"{}" - {} views').format(most_popular_articles[0][0], most_popular_articles[0][1])
-print('"{}" - {} views').format(most_popular_articles[1][0], most_popular_articles[1][1])
-print('"{}" - {} views').format(most_popular_articles[2][0], most_popular_articles[2][1])
+output.write('The most popular three articles of all time are: \n')
+output.write('* "' + most_popular_articles[0][0] + '" - ')
+output.write(str('{:,}'.format(most_popular_articles[0][1])) + ' views\n')
+output.write('* "' + most_popular_articles[1][0] + '" - ')
+output.write(str('{:,}'.format(most_popular_articles[1][1])) + ' views\n')
+output.write('* "' + most_popular_articles[2][0] + '" - ')
+output.write(str('{:,}'.format(most_popular_articles[2][1])) + ' views\n')
 
 # PART TWO: Who are the most popular article authors of all time?
-print('\nThe most popular article authors of all time are: ')
-author_views = 'SELECT * FROM author_views'
+author_views = 'SELECT * FROM author_views;'
 most_popular_authors = get_query_results(author_views)
-print('"{}" - {} views').format(most_popular_authors[0][0], most_popular_authors[0][1])
-print('"{}" - {} views').format(most_popular_authors[1][0], most_popular_authors[1][1])
-print('"{}" - {} views').format(most_popular_authors[2][0], most_popular_authors[2][1])
-print('"{}" - {} views').format(most_popular_authors[3][0], most_popular_authors[3][1])
+output.write('\nThe most popular article authors of all time are: \n')
+output.write('* ' + most_popular_authors[0][0] + ' - ')
+output.write(str('{:,}'.format(most_popular_authors[0][1])) + ' views\n')
+output.write('* ' + most_popular_authors[1][0] + ' - ')
+output.write(str('{:,}'.format(most_popular_authors[1][1])) + ' views\n')
+output.write('* ' + most_popular_authors[2][0] + ' - ')
+output.write(str('{:,}'.format(most_popular_authors[2][1])) + ' views\n')
+output.write('* ' + most_popular_authors[3][0] + ' - ')
+output.write(str('{:,}'.format(most_popular_authors[3][1])) + ' views\n')
+
+# PART THREE: On which days did more than 1% of requests lead to errors?
+error_percentage = ('SELECT TO_CHAR(day, \'fmMonth dd, YYYY\'), percentage'
+                    ' FROM error_percentage WHERE percentage > 1;')
+most_error_days = get_query_results(error_percentage)
+output.write('\nThe days on which more than 1% of requests')
+output.write('lead to errors are: \n')
+output.write('* ' + str(most_error_days[0][0]) + ' - ')
+output.write(str(round(most_error_days[0][1], 1)) + '% errors')
+
+# closes the output text file
+output.close()
